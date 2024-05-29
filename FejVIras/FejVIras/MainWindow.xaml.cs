b1a4e533dab2bfac5d5b1d0b2882a4240ae3b5da
Zantai <bencee2004@gmail.com>
2024-05-29 21:11:35 +0200

    Add files via upload

---
+++ FejVIras/FejVIras/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FejVIras
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string nv;
+        public int penz;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void be_Click(object sender, RoutedEventArgs e)
+        {
+            nv = nev.Text;
+            penz = int.Parse(egy.Text);
+            nh.Text = nv;
+            eh.Text = "Egyenleged: " + penz+"ft";
+            be.IsEnabled = false;
+            nev.IsEnabled = false;
+            egy.IsEnabled = false;
+            tet.IsEnabled = true;
+            tipp.IsEnabled = true;
+            game.IsEnabled = true;
+
+        }
+
+        private void game_Click(object sender, RoutedEventArgs e)
+        {
+            if (penz >= 1)
+            {
+                Random r = new Random();
+                int a = r.Next(1, 3);
+                string enemy = "";
+                if (a == 1)
+                    enemy = "fej";
+                else
+                    enemy = "írás";
+                fin.Text = enemy;
+                if (enemy == tipp.Text)
+                {
+                    MessageBox.Show("Win");
+                    penz += int.Parse(tet.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Lose");
+                    penz-= int.Parse(tet.Text);
+                }
+                eh.Text = "Egyenleged: " + penz + "ft";
+            }
+            else
+            {
+                MessageBox.Show("Nincs pénzed!");
+                this.Close();
+            }
+        }
+    }
+}
---
+++ SzGep/SzGep/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SzGep
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double fin = 0;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            ere.Text=fin + "";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                int a = int.Parse(szam.Text);
+            }
+            catch {
+                MessageBox.Show("Nem szám!");
+                szam.Clear();
+            }
+        }
+
+        public void Muv(string mv)
+        {
+            if (mv == "+")
+            {
+                fin += double.Parse(szam.Text);
+                ere.Text = fin + "";
+            }else if (mv == "-")
+            {
+                fin -= double.Parse(szam.Text);
+                ere.Text = fin + "";
+            }
+            else if (mv == "*")
+            {
+                fin *= double.Parse(szam.Text);
+                ere.Text = fin + "";
+            }
+            else if (mv == "/")
+            {
+                fin /= double.Parse(szam.Text);
+                ere.Text = fin + "";
+            }
+            else if (mv == "%")
+            {
+                fin %= double.Parse(szam.Text);
+                ere.Text = fin + "";
+            }
+            else if (mv == "C")
+            {
+                fin = 0;
+                ere.Text = fin + "";
+                szam.Clear();
+            }
+
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("+");
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("-");
+        }
+
+        private void szor_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("*");
+        }
+
+        private void oszt_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("/");
+        }
+
+        private void mar_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("%");
+        }
+
+        private void res_Click(object sender, RoutedEventArgs e)
+        {
+            Muv("C");
+        }
+    }
+}
